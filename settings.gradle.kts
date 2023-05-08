a6e7981633730df4bbe2a2abbc22a6ea5e8d2d76
KonstantinKhan <khan366kos@gmail.com>
2023-05-08 10:45:17 +0500

    add gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "shopping_list_bot"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
+}
+
+include("shopping_list_bot-be-ktor-app")
+include("shopping_list-be-lib-telegram-core")
+include("shopping_list-be-common")
+include("shopping_list-be-lib-telegram-api")
+include("shopping_list-be-mapping")
+include("shopping_list-be-repo_in_memory")
+include("shopping_list-be-shopping_list-service")
+include("shopping_list-be-shopping_list-logic")
+include("shopping_list-cor")
+include("shopping_list-be-sql")
+include("shopping_list-be-repo-test")
+include("ktglib-transport-models")
