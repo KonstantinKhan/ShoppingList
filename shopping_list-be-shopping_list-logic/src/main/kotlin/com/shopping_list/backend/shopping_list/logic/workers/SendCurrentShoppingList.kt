15355793b298de751a7046b878e1ab2176b61804
KonstantinKhan <khan366kos@gmail.com>
2023-06-11 15:15:11 +0500

    updated the sendCurrentShoppingList method

--- shopping_list-be-shopping_list-logic/src/main/kotlin/com/shopping_list/backend/shopping_list/logic/workers/SendCurrentShoppingList.kt
+++ shopping_list-be-shopping_list-logic/src/main/kotlin/com/shopping_list/backend/shopping_list/logic/workers/SendCurrentShoppingList.kt
@@ -1,12 +1,17 @@
 package com.shopping_list.backend.shopping_list.logic.workers
 
 import com.shopping_list.common.context.BeContext
+import com.shopping_list.common.models.MessageId
 import ru.fit_changes.cor.CorChainDsl
 import ru.fit_changes.cor.worker
+import ru.ktglib.types.Message
 
 fun CorChainDsl<BeContext>.sendCurrentShoppingList(title: String) = worker {
     this.title = title
+    on { errors.isEmpty() }
     handle {
-//        messageId = httpClient.sendCurrentShoppingList(this).result.messageId
+        httpClient.sendCurrentShoppingList(this).result?.let {
+            messageId = MessageId((it as Message).messageId)
+        }
     }
 }
