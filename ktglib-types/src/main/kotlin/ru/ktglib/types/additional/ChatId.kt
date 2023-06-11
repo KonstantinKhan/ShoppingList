678422543a14012da92920f1ed2a997615c8c268
KonstantinKhan <khan366kos@gmail.com>
2023-06-11 15:03:51 +0500

    replaced value class to data class

--- ktglib-types/src/main/kotlin/ru/ktglib/types/additional/ChatId.kt
+++ ktglib-types/src/main/kotlin/ru/ktglib/types/additional/ChatId.kt
@@ -3,9 +3,8 @@ package ru.ktglib.types.additional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@JvmInline
 @Serializable
-value class ChatId(
+data class ChatId(
     @SerialName("chat_id")
     private val id: String
 ) {
